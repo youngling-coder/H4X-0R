b6be6285fc8eeee2c765976199ad7dd3ea206d24
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2025-02-10 15:10:19 +0100

    rewrite instructions obtaining to the separate function

--- llm.py
+++ llm.py
@@ -1,23 +1,30 @@
-import os
 import logging
-from typing import Optional, Iterable
+from typing import Optional
 
+from settings import h4x0r_settings
 import google.generativeai as genai
 from google.generativeai.generative_models import ChatSession
 
 from dump_file import write_dump_file, read_dump_file
 
-try:
-    with open("instructions.md", "r") as instructions_file:
-        INSTRUCTIONS = instructions_file.read()
+def get_instructions() -> str | None:
+    
+    instructions = None
 
-except Exception as e:
-    logging.error(f"An error occurred while reading H4X-0R instrucions: {e}")
+    try:
+        with open("instructions.md", "r") as instructions_file:
+            instructions = instructions_file.read()
 
 
-genai.configure(api_key=os.getenv("GOOGLE_API"))
+    except Exception as e:
+        logging.error(f"An error occurred while reading H4X-0R instrucions: {e}")
 
-model = genai.GenerativeModel(model_name="gemini-2.0-flash")
+    finally:
+        return instructions
+
+genai.configure(api_key=h4x0r_settings.GOOGLE_API)
+
+model = genai.GenerativeModel(model_name=h4x0r_settings.LLM_NAME)
 
 
 def get_chat(title: str) -> Optional[ChatSession]:
@@ -35,21 +42,21 @@ def get_chat(title: str) -> Optional[ChatSession]:
 
 def create_new_chat(title: str, type_: str, participants: dict) -> ChatSession:
 
-    global INSTRUCTIONS
-
-    INSTRUCTIONS += f"""
+    instructions = get_instructions()
+    instructions += f"""
 ### This chat type is a: {type_}
 ### This chat is with users: {", ".join([username for username in participants.keys()])}
 ### You should call him/her: {", ".join([participants[username] for username in participants.keys()])}
 (If the user's full name is not provided, you can but NOT required to politely ask for user's name)
 """
+    
 
     chat = model.start_chat(
         history=[
             {
                 "role": "user",
                 "parts": [
-                    INSTRUCTIONS,
+                    instructions,
                 ],
             },
             {
