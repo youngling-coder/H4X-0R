e2c62d8ca38a5e13e0a8c52565f4e881dc76b1d2
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2025-02-10 17:23:52 +0100

    add a function to trim history in order to save resources

--- func.py
+++ func.py
@@ -1,7 +1,10 @@
-import re
 import aiohttp
+import sys
+import re
 import io
+
 from PIL import Image
+from google.generativeai.generative_models import ChatSession
 from aiogram.types.photo_size import PhotoSize
 
 from settings import h4x0r_settings
@@ -20,11 +23,24 @@ async def photo_to_pil_object(photo: PhotoSize):
 
     file_info = await H4X0R_bot.get_file(photo.file_id)
     file_url = f"https://api.telegram.org/file/bot{h4x0r_settings.TELEGRAM_BOT_TOKEN}/{file_info.file_path}"
+    print(file_url)
 
     async with aiohttp.ClientSession() as session:
         async with session.get(file_url) as response:
             image_bytes = await response.read()
 
     image = Image.open(io.BytesIO(image_bytes))
-    
+
     return image
+
+
+def truncate_history(chat_object: ChatSession):
+    
+    while (
+        sum(sys.getsizeof(str(item)) for item in chat_object.history)
+        > h4x0r_settings.DUMP_FILE_MAXIMUM_SIZE_BYTES
+        or len(chat_object.history) > h4x0r_settings.DUMP_FILE_MAXIMUM_ITEMS_COUNT
+    ):
+        if not chat_object.history:
+            break
+        chat_object.history.pop(0)
