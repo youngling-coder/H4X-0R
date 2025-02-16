a0b7a5ad3aa7e350a6b79044e52988df8e2a3db8
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2025-02-16 12:26:37 +0100

    delete keyboards.py

--- keyboards.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-
-enable_button = KeyboardButton(text="🟢 Enable Offline Mode")
-disable_button = KeyboardButton(text="🔴 Disable Offline Mode")
-
-menu_keyboard_markup = ReplyKeyboardMarkup(
-    keyboard=[[enable_button], [disable_button]], resize_keyboard=True
-)
