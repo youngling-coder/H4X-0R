8eefef0d87b614d8e8489297e1dcb2d2da70fd00
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2025-03-02 12:19:52 +0100

    add advanced filter of replies and names command in order to show how bot can be called

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
+import random
+
 from aiogram import Router, Bot
-from aiogram import types
+from aiogram import types, F
 from aiogram.enums.parse_mode import ParseMode
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Filter, Command
 from aiogram.client.default import DefaultBotProperties
-from youtubesearchpython import VideosSearch
 
 from func import escape_markdown, photo_to_pil_object
 from settings import h4x0r_settings
@@ -17,6 +18,20 @@ H4X0R_bot = Bot(
 
 router = Router()
 
+is_activated = lambda message: message and list(
+    filter(lambda x: message.lower().startswith(x), h4x0r_settings.BOT_NAMES)
+)
+
+
+class ReplyToUserFilter(Filter):
+    def __init__(self, username: str):
+        self.username = username
+
+    async def __call__(self, message: types.Message) -> bool:
+        if message.reply_to_message and message.reply_to_message.from_user:
+            return message.reply_to_message.from_user.username == self.username
+        return False
+
 
 @router.message(CommandStart())
 async def command_start_handler(message: types.Message) -> None:
@@ -26,7 +41,6 @@ async def command_start_handler(message: types.Message) -> None:
         if message.from_user.username
         else message.from_user.id
     )
-    full_name = f"{message.chat.first_name} {message.chat.last_name}"
 
     if username == h4x0r_settings.OWNER_USERNAME:
         chat = get_chat(title=username)
@@ -46,19 +60,47 @@ async def command_start_handler(message: types.Message) -> None:
         )
 
 
-@router.message()
+@router.message(Command("names"))
+async def what_is_my_name(message: types.Message):
+
+    await message.reply(
+        "These are some of my names: "
+        + ", ".join(
+            [
+                choice.capitalize()
+                for choice in random.choices(
+                    h4x0r_settings.BOT_NAMES, k=random.randint(3, 6)
+                )
+            ]
+        )
+    )
+
+
+@router.message(ReplyToUserFilter("h4x0r_hector_bot"))
 async def message_handler(message: types.Message) -> None:
+    await get_answer(message=message)
 
-    is_content_appropriate = lambda message: message.text or message.caption or message.photo
+
+@router.message(
+    lambda message: (is_activated(message.caption) or is_activated(message.text))
+)
+async def message_handler(message: types.Message) -> None:
+    await get_answer(message=message)
+
+
+async def get_answer(message: types.Message):
+    is_content_appropriate = (
+        lambda message: message.text or message.caption or message.photo
+    )
     is_group = lambda message: message.chat.type in ("group", "supergroup")
-    is_activated = lambda message:  message and list(filter(lambda x: message.lower().startswith(x), 
-                                               ("hector", "гектор", "железяка", "байтоголовый", "битоголовый", "бот")))
 
-    print(is_activated(message.caption), is_activated(message.text))
+    if (
+        is_content_appropriate(message)
+        and is_group(message)
+        and (F.reply_to_message)
+        or (is_activated(message.caption) or is_activated(message.text))
+    ):
 
-    if is_content_appropriate(message) and is_group(message) and \
-    (is_activated(message.caption) or is_activated(message.text)):
-        
         title = message.chat.id
         chat = get_chat(title=title)
 
@@ -84,6 +126,4 @@ async def message_handler(message: types.Message) -> None:
             message=final_message, chat_name=str(message.chat.id), chat_object=chat
         )
 
-        await message.reply(
-            escape_markdown(response)
-        )
+        await message.reply(escape_markdown(response))
