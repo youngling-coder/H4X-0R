72da1e4be9c2fed8cbc58d19f4ccbb43887a0ba4
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2025-03-17 15:55:59 +0100

    update code to work with database instead of pickle files

--- llm.py
+++ llm.py
@@ -1,13 +1,11 @@
 import logging
-import sys
 from typing import Optional
 
 from func import truncate_history
 from settings import h4x0r_settings
 import google.generativeai as genai
 from google.generativeai.generative_models import ChatSession
-
-from dump_file import write_dump_file, read_dump_file
+import crud
 
 
 def get_instructions() -> str | None:
@@ -30,20 +28,21 @@ genai.configure(api_key=h4x0r_settings.GOOGLE_API)
 model = genai.GenerativeModel(model_name=h4x0r_settings.LLM_NAME)
 
 
-def get_chat(title: str) -> Optional[ChatSession]:
+def get_chat(chat_id: str) -> Optional[ChatSession]:
     try:
-        history = read_dump_file(title)
+        history = crud.get_chat_history(chat_id)
+
         if history:
             chat = model.start_chat(history=history)
 
             return chat
     except Exception as e:
-        logging.error(f"Couldn't read chat object with title {title}: {e}")
+        logging.error(f"Couldn't read chat object with title {chat_id}: {e}")
 
     return
 
 
-def create_new_chat(title: str) -> ChatSession:
+def create_new_chat(chat_id: str) -> ChatSession:
 
     instructions = get_instructions()
 
@@ -58,13 +57,14 @@ def create_new_chat(title: str) -> ChatSession:
         ]
     )
 
-    write_dump_file(title, chat.history)
+    crud.create_chat(chat_id=chat_id)
+    crud.create_message(chat_id=chat_id, content=instructions, role="user")
 
     return chat
 
 
 async def respond_on_message(
-    message: list, chat_name: str, chat_object: ChatSession
+    message: list, chat_id: str, chat_object: ChatSession
 ) -> str:
     try:
         response = await chat_object.send_message_async(content=message)
@@ -74,7 +74,9 @@ async def respond_on_message(
 
         truncate_history(chat_object)
 
-        write_dump_file(chat_name, chat_object.history)
+        crud.create_message(chat_id, content=message[0], role="user")
+        crud.create_message(chat_id, content=response.text, role="model")
+
         return response.text
 
     except Exception as e:
