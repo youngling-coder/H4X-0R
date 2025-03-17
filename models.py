9a3b47e3277a2aa2a830d79c84dae3d0b2ae5e16
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2025-03-17 15:47:37 +0100

    add initial database sqlalchemy models

---
+++ models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import String, Integer, ForeignKey, Enum
+from sqlalchemy.orm import mapped_column, Mapped, declarative_base, relationship
+import enum
+
+Base = declarative_base()
+
+class UserRole(enum.Enum):
+    user = "user"
+    model = "model"
+
+
+class Chat(Base):
+    __tablename__ = "chats"
+
+    id: Mapped[str] = mapped_column(String, nullable=False, unique=True, primary_key=True)
+    messages: Mapped[list["Message"]] = relationship("Message", back_populates="chat", cascade="all, delete")
+
+
+class Message(Base):
+    __tablename__ = "messages"
+
+    id: Mapped[int] = mapped_column(Integer, nullable=False, unique=True, primary_key=True)
+    chat_id: Mapped[str] = mapped_column(ForeignKey("chats.id", ondelete="CASCADE"))
+    chat: Mapped["Chat"] = relationship("Chat", back_populates="messages")
+    content: Mapped[str] = mapped_column(String, nullable=False)
+    role: Mapped[UserRole] = mapped_column(Enum(UserRole), nullable=False)
+    
