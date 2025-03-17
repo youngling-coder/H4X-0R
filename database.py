db6406113f54f98abab0d7819a49e073d59dce94
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2025-03-17 15:46:53 +0100

    setup database

---
+++ database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from settings import h4x0r_settings
+
+DATABASE_URL = f"sqlite:///{h4x0r_settings.DATABASE_FILE}"
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False}, echo=False)
+
+SessionLocal = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+
+
+# Dependency for getting DB session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
