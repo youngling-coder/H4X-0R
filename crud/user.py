6adb1bae137daba54fdb88ff760a8e7ec4b61b8d
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2025-03-23 14:31:45 +0100

    separate create_user function and add user_update functionality

--- crud/user.py
+++ crud/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import select
+from sqlalchemy import select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import db_session_required
@@ -30,17 +30,38 @@ async def get_user_by_id(user_id: int, db: AsyncSession):
 
 
 @db_session_required
-async def create_user_if_not_exists(user_schema: schemas.User, db: AsyncSession):
+async def create_user(user_schema: schemas.User, db: AsyncSession):
+
+    user = models.User(**user_schema.model_dump())
+
+    db.add(user)
+    await db.commit()
+    await db.refresh(user)
+    await db.close()
+
+    return user
+
+
+@db_session_required
+async def create_user_if_not_exists_update_otherwise(user_schema: schemas.User, db: AsyncSession):
 
     user = await get_user_by_telegram_id(user_schema.telegram_id)
 
     if not user:
+        await create_user(user_schema)
 
-        user = models.User(**user_schema.model_dump())
-
-        db.add(user)
+    else:
+        stmt = (
+                update(models.User)
+                .where(models.User.telegram_id == user_schema.telegram_id)
+                .values(**user_schema.model_dump())
+                .returning(models.User)
+            )
+        result = await db.execute(stmt)
+        user = result.scalars().first()
         await db.commit()
-        await db.refresh(user)
-        await db.close()
+
+
+    await db.close()
 
     return user
