c3a1ec05d6277b39fe3b7a4016253bb6825ffcc0
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2025-03-23 14:30:48 +0100

    add username field to the user pydantic schema

--- schemas/user.py
+++ schemas/user.py
@@ -1,7 +1,9 @@
 from pydantic import BaseModel
+from typing import Optional
 
 
 class User(BaseModel):
     telegram_id: int
     reputation: float = 0.0
     name: str
+    username: Optional[str] = None
